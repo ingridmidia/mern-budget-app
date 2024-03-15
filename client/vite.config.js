e1b4e2aa2e942dd14290e42a73c0086b0e11a6ce
ingridmidia <ingridmidia@gmail.com>
2024-03-15 15:10:47 -0500

    pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "/public/basilicon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#3c5318",
+      },
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
     proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
+      "/graphql": {
+        target: "http://localhost:3001",
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
