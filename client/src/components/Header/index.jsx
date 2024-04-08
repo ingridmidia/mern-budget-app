75265fa52b95c3069940cab4ace26c3e906dc4e0
ingridmidia <ingridmidia@gmail.com>
2024-04-08 18:59:29 -0500

    change design

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,34 +1,32 @@
-import { Typography } from '@mui/material';
-import { Box, flexbox, fontFamily, typography } from '@mui/system';
-import React from 'react';
-import { Animated } from 'react-animated-css';
-import '@fontsource/lobster-two';
-import Banner from '../../assets/Images/BasilHeader.png';
-
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { Animated } from "react-animated-css";
+import "@fontsource/lobster-two";
 
 const Header = () => {
-  const CustomGrn = '#2a3a10';
+  const backgroundColor = "#578d13";
+  const titleColor = "#3a302a";
   return (
-   
-      <Box
-        style={{
-          backgroundImage: `url(${Banner})`,
-          backgroundSize: 'cover', // Adjust as needed
-          backgroundPosition: 'center', // Adjust as needed
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Animated animationIn='fadeInRightBig'>
-        <Typography variant='h1' fontFamily='Lobster Two, cursive' style={{ color: CustomGrn }}>
-  Basil
-</Typography>
-
-        </Animated>
-      </Box>
-   
+    <Box
+      style={{
+        backgroundColor,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Animated animationIn="fadeInRightBig">
+        <Typography
+          variant="h1"
+          fontFamily="Lobster Two, cursive"
+          style={{ color: titleColor }}
+        >
+          Basil
+        </Typography>
+      </Animated>
+    </Box>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,19 @@
-
-import React from 'react';
-import { Box, Grid, Paper, Typography, Button, ThemeProvider } from '@mui/material';
-import LapTop from '../assets/Images/Basil-laptop.png';
-import PlantGif from '../assets/Images/KatieVosPlant.gif';
+import React from "react";
+import { Box, Grid, Paper, Typography, Button } from "@mui/material";
+import LapTop from "../assets/Images/Basil-laptop.png";
 
 const Home = () => {
   return (
-    <>
-      <Grid container justifyContent="center">
-        <Grid item xs={12} m={6}>
+    <Box sx={{ padding: 4 }}>
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item xs={12} md={6}>
           <Box
             sx={{
-              display: 'flex',
-              flexDirection: { xs: 'column-reverse', sm: 'row' }, // Adjust for different screen sizes
-              alignItems: { xs: 'center', sm: 'space-Around' }, // Center items for xs, align to the start for sm
-              marginBottom: 2,
+              display: "flex",
+              flexDirection: { xs: "column", sm: "row" },
+              alignItems: "center",
+              gap: 2,
+              marginBottom: { xs: 2, sm: 0 },
             }}
           >
             {/* Image */}
@@ -22,81 +21,97 @@ const Home = () => {
               src={LapTop}
               alt="laptop open to Basil"
               style={{
-                width: '100%',
+                width: "100%",
                 maxWidth: 400,
-                height: 'auto',
-                objectFit: 'cover',
+                height: "auto",
+                objectFit: "cover",
               }}
-
             />
 
             {/* Text Box */}
-            <Box alignContent={'end'}
+            <Box
               sx={{
-                textAlign: { xs: 'center', sm: 'end' },
-                marginTop: { xs: 2, sm: 0 },
+                textAlign: "center",
+                padding: 2,
               }}
             >
-              <Paper elevation={3} style={{ maxHeight: '100vh', overflow: 'auto', padding: 3 }}>
-                <Typography variant='h4' style={{ fontFamily: 'Lobster Two, sans-serif' }}>👋 Hey there! We are Basil.</Typography>
-                <Typography variant='body' style={{ fontFamily: 'Andika, sans-serif' }}> We love helping people take charge of their financial future!
+              <Paper elevation={3} style={{ padding: 3 }}>
+                <Typography
+                  variant="h4"
+                  style={{ fontFamily: "Lobster Two, sans-serif" }}
+                >
+                  Hey there! We are Basil.
+                </Typography>
+                <Typography
+                  variant="body"
+                  style={{ fontFamily: "Andika, sans-serif" }}
+                >
+                  We love helping people take charge of their financial future!
                   <br />
-                  Interested?  Learn more aboout us <a href='/about'><Button variant='contained' color='success' >HERE</Button> </a></Typography>
+                  Interested? Learn more about us{" "}
+                  <a href="/about">
+                    <Button variant="contained" color="success">
+                      HERE
+                    </Button>{" "}
+                  </a>
+                </Typography>
               </Paper>
             </Box>
-            {/* Image */}
-            <img
-              src={PlantGif}
-              alt="Illustrated Gif of plants"
-              style={{
-                width: '100%',
-                maxWidth: 400,
-                height: 'auto',
-                objectFit: 'cover',
-              }}
-
-            />
           </Box>
         </Grid>
       </Grid>
 
-      <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      pb: 3
-      }} 
-   >
-      <Typography variant='h3' style={{ fontFamily: 'lobster Two, sans-serif', }}>
-        Check out what Basil users are saying!
-      </Typography>
-    </Box >
-
-      <Grid container spacing={{ xs: 2, md: 3, xl: 4 }} columns={{ xs: 2, md: 6, xl: 12 }} justifyContent={'center'}>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: 'wrap',
-            justifyContent: 'end', // Center the items horizontally
-            textAlign: 'center',
-            '& > :not(style)': {
-
-              m: 4,
-              p: 2,
-              width: 260,
-              height: 255,
-
-            },
-          }}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          pb: 3,
+        }}
+      >
+        <Typography
+          variant="h3"
+          style={{ fontFamily: "Lobster Two, sans-serif" }}
         >
-          <Paper varient='elevation' style={{ fontFamily: 'Andika, sans-serif' }}>"Absolutely love this budgeting app! It's user-friendly, visually appealing, and has transformed my financial habits. The intuitive features make budgeting a breeze, providing insightful analytics. A must-have for anyone seeking financial control!" - Sophia R.</Paper>
-          <Paper varient='elevation' style={{ fontFamily: 'Andika, sans-serif' }}>"Impressive! This app simplifies budgeting like never before. Alex-friendly interface, powerful tools, and insightful reports. My financial game has never been stronger!" - Alex T.</Paper>
-          <Paper varient='elevation' style={{ fontFamily: 'Andika, sans-serif' }}>"Five stars for this brilliant budgeting app! Its simplicity and effectiveness makes using it FUN! From tracking expenses to setting goals, it's the financial companion everyone needs. A total game-changer!" -Olivia H.</Paper>
-          <Paper varient='elevation' style={{ fontFamily: 'Andika, sans-serif' }}>"Basil changed my life! This app is a game-changer. It effortlessly guides you through budgeting, offers clear insights, and motivates financial growth. A must for stress-free money management!" -Emily C.</Paper>
-        </Box>
+          Check out what Basil users are saying!
+        </Typography>
+      </Box>
+
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={10} md={5} lg={3}>
+          <Paper elevation={3} style={{ padding: 2, textAlign: "center" }}>
+            "Absolutely love this budgeting app! It's user-friendly, visually
+            appealing, and has transformed my financial habits. The intuitive
+            features make budgeting a breeze, providing insightful analytics. A
+            must-have for anyone seeking financial control!" - Sophia R.
+          </Paper>
+        </Grid>
+        <Grid item xs={10} md={5} lg={3}>
+          <Paper elevation={3} style={{ padding: 2, textAlign: "center" }}>
+            "Impressive! This app simplifies budgeting like never before.
+            Alex-friendly interface, powerful tools, and insightful reports. My
+            financial game has never been stronger!" - Alex T.
+          </Paper>
+        </Grid>
+        <Grid item xs={10} md={5} lg={3}>
+          <Paper elevation={3} style={{ padding: 2, textAlign: "center" }}>
+            "Five stars for this brilliant budgeting app! Its simplicity and
+            effectiveness makes using it FUN! From tracking expenses to setting
+            goals, it's the financial companion everyone needs. A total
+            game-changer!" - Olivia H.
+          </Paper>
+        </Grid>
+        <Grid item xs={10} md={5} lg={3}>
+          <Paper elevation={3} style={{ padding: 2, textAlign: "center" }}>
+            "Basil changed my life! This app is a game-changer. It effortlessly
+            guides you through budgeting, offers clear insights, and motivates
+            financial growth. A must for stress-free money management!" - Emily
+            C.
+          </Paper>
+        </Grid>
       </Grid>
-   </>
+    </Box>
   );
 };
 
