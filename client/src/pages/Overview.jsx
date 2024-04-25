ea2c669ed4546fe958917b8df7b6729482e1ca8a
ingridmidia <ingridmidia@gmail.com>
2024-04-25 11:48:10 -0500

    overview page

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -7,28 +7,28 @@ import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import Hello from "../components/Hello";
 import MakeExtraBar from "../components/graphs/extraGraph/extraGraph";
-import { Grid, Paper, Box } from "@mui/material";
+import { Grid, Paper} from "@mui/material";
 
 const Overview = () => {
   // Check if the user is logged in
-  if (!Auth.loggedIn()) {
-    return (
-      <>
-        <Grid container justifyContent="space-around" sx={{ marginTop: 2 }}>
-          <a href="/Login" style={{ textDecoration: "none" }}>
-            <Button variant="contained" color="success">
-              Log In
-            </Button>
-          </a>
-          <a href="/Signup" style={{ textDecoration: "none" }}>
-            <Button variant="contained" color="success">
-              Sign Up
-            </Button>
-          </a>
-        </Grid>
-      </>
-    );
-  }
+  // if (!Auth.loggedIn()) {
+  //   return (
+  //     <>
+  //       <Grid container justifyContent="space-around" sx={{ marginTop: 2 }}>
+  //         <a href="/Login" style={{ textDecoration: "none" }}>
+  //           <Button variant="contained" color="success">
+  //             Log In
+  //           </Button>
+  //         </a>
+  //         <a href="/Signup" style={{ textDecoration: "none" }}>
+  //           <Button variant="contained" color="success">
+  //             Sign Up
+  //           </Button>
+  //         </a>
+  //       </Grid>
+  //     </>
+  //   );
+  // }
 
   const addExpense = (event) => {
     event.preventDefault();
@@ -59,20 +59,12 @@ const Overview = () => {
   const containerStyle = {
     border: "1px solid #ddd",
     borderRadius: "8px",
-    marginBottom: "20px",
-    padding: "20px",
+    padding: "0.5rem",
     marginTop: "20px",
     background: "white",
     display: "flex",
     justifyContent: "center",
-  };
-
-  const buttoncontainer = {
-    display: "flex",
-    justifyContent: "center",
     alignItems: "center",
-    marginBottom: "20px",
-    gap: "50px",
   };
 
   return (
@@ -86,43 +78,51 @@ const Overview = () => {
           padding: "2rem",
           width: "50%",
           margin: "auto",
-          backgroundColor:"#BBEDC4",
+          backgroundColor: "#BBEDC4",
           marginTop: "2rem",
-          borderRadius: "1rem"
-          
+          borderRadius: "1rem",
         }}
       >
         <Hello userInfo={userInfo} />
+        <div className="container" style={containerStyle}>
+          <Button
+            variant="outlined"
+            color="success"
+            style={{
+              fontFamily: "andika, sans-serif",
+              color: "#365242",
+              fontWeight: "bold",
+              marginRight: "1rem",
+            }}
+            startIcon={<KeyboardDoubleArrowUpIcon />}
+            onClick={(event) => addIncome(event)}
+          >
+            Add Income
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+            style={{
+              fontFamily: "andika, sans-serif",
+              color: "#365242",
+              fontWeight: "bold",
+              marginLeft: "1rem",
+            }}
+            startIcon={<KeyboardDoubleArrowDownIcon />}
+            onClick={(event) => addExpense(event)}
+          >
+            Add Expense
+          </Button>
+        </div>
+        <Grid container justifyContent="center" style={{ maxHeight: "50vh" }}>
+          <Paper elevation={24} style={{ width: "50%", padding: "16px" }}>
+            <MakeExtraBar
+              style={{ width: "100%", fontFamily: "andika, sans-serif" }}
+              justifyContent="center"
+            />
+          </Paper>
+        </Grid>
       </div>
-
-      <div className="container" style={buttoncontainer}>
-        <Button
-          variant="outlined"
-          color="success"
-          style={{ marginTop: "16px", fontFamily: "andika, sans-serif" }}
-          startIcon={<KeyboardDoubleArrowUpIcon />}
-          onClick={(event) => addIncome(event)}
-        >
-          Add Income
-        </Button>
-        <Button
-          variant="outlined"
-          color="success"
-          style={{ marginTop: "16px", fontFamily: "andika, sans-serif" }}
-          startIcon={<KeyboardDoubleArrowDownIcon />}
-          onClick={(event) => addExpense(event)}
-        >
-          Add Expense
-        </Button>
-      </div>
-      <Grid container justifyContent="center" style={{ maxHeight: "50vh" }}>
-        <Paper elevation={24} style={{ width: "50%", padding: "16px" }}>
-          <MakeExtraBar
-            style={{ width: "100%", fontFamily: "andika, sans-serif" }}
-            justifyContent="center"
-          />
-        </Paper>
-      </Grid>
     </>
   );
 };
