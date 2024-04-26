b0cbe75852b4d3b4e458f65bf2eac07eceb1f4c5
ingridmidia <ingridmidia@gmail.com>
2024-04-26 16:51:55 -0500

    wip: homepage design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,74 +5,51 @@ import image from "../assets/Images/basil.png";
 const Home = () => {
   return (
     <>
-      <Grid container justifyContent="center">
-        {/* Image */}
-        <Grid item xs={12} md={6}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <img
-              src={image}
-              alt="laptop open to Basil"
+      <div id="bg">
+        <Paper elevation={3} style={{ padding: 6, margin: "2rem", }}>
+          <Box
+            sx={{
+              padding: 2,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <Typography
+              variant="h3"
               style={{
-                width: "100%",
-                maxWidth: 300,
-                height: "auto",
-                objectFit: "cover",
-                borderRadius: "10px",
-                margin: "2rem",
-                justifyContent: "center",
-                alignSelf:  "center"
-              }} id="laptot-image"
-            />
-          </div>
-        </Grid>
-
-        {/* Text Box */}
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3} style={{ padding: 6, margin: "2rem" }}>
-            <Box
-              sx={{
-                padding: 2,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
+                fontFamily: "Andika, sans-serif",
+                textAlign: "center",
+                padding: "1rem",
               }}
             >
-              <Typography
-                variant="h3"
-                style={{
-                  fontFamily: "Andika, sans-serif",
-                  textAlign: "center",
-                  padding: "1rem",
-                }}
-              >
-                Hey, there!
-                <br /> We are Basil.
-              </Typography>
-              <Typography
-                variant="body1"
-                style={{
-                  fontFamily: "Andika, sans-serif",
-                  textAlign: "center",
-                  paddingBottom: "1rem",
-                }}
-              >
-                We love helping people take charge of their financial future!
-                <br />
-                Interested?
-                <br />
-                Learn more about us{" "}
-                <a href="/about">
-                  <Button variant="contained" color="success">
-                    HERE
-                  </Button>{" "}
-                </a>
-              </Typography>
-            </Box>
-          </Paper>
-        </Grid>
-      </Grid>
+              Hey, there!
+              <br /> We are Basil.
+            </Typography>
+            <Typography
+              variant="body1"
+              style={{
+                fontFamily: "Andika, sans-serif",
+                textAlign: "center",
+                paddingBottom: "1rem",
+              }}
+            >
+              We love helping people take charge of their financial future!
+              <br />
+              Interested?
+              <br />
+              Learn more about us{" "}
+              <a href="/about">
+                <Button variant="contained" color="success">
+                  HERE
+                </Button>{" "}
+              </a>
+            </Typography>
+          </Box>
+        </Paper>
+      </div>
 
       <Box sx={{ padding: 4 }}>
         <Box
